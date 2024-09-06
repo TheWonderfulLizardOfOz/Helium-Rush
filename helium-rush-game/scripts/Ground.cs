fa433809e4cd8ee48082fa18ae39d574259c6fe4
Garance AR <79841946+TheWonderfulLizardOfOz@users.noreply.github.com>
2024-09-06 18:33:08 +0100

    Add pawn navigation (placeholder)

---
+++ helium-rush-game/scripts/Ground.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class Ground : TileMapLayer
+{
+	[Export]
+	private TileMapLayer obstacleLayer;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+    public override bool _UseTileDataRuntimeUpdate(Vector2I coords)
+    {
+      Godot.Collections.Array<Vector2I> cells = obstacleLayer.GetUsedCells();
+			foreach (Vector2I cell in cells) 
+			{
+				if (cell == coords) 
+				{
+					return true;
+				}
+			}
+			return false;
+    }
+
+    public override void _TileDataRuntimeUpdate(Vector2I coords, TileData tileData)
+    {
+      Godot.Collections.Array<Vector2I> cells = obstacleLayer.GetUsedCells();
+
+			foreach(Vector2I cell in cells) 
+			{
+				if (cell == coords)
+				{
+					tileData.SetNavigationPolygon(0, null);
+				}
+			}
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+		{
+		}
+}
--- helium-rush-game/scripts/Pawn.cs
+++ helium-rush-game/scripts/Pawn.cs
@@ -1,25 +1,36 @@
 using Godot;
 using System;
+using System.Numerics;
+using Vector2 = Godot.Vector2;
 
 public partial class Pawn : Node2D
 {
 	[Export]
 	int sleepDecayRate = 10;
 
+	NavigationAgent2D navigationAgent;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		var timer = GetNode<Timer>("/root/Base/TickTimer");
+		navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
 		timer.Timeout += OnTick;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		navigationAgent.TargetPosition = GetGlobalMousePosition();
 	}
 
 	public void OnTick()
-	{
-		
+	{	
+		Vector2 delta;
+		Vector2 nextPathPosition = navigationAgent.GetNextPathPosition();
+		//Translate(nextPathPosition - currentPosition);
+		delta.X = (int)Math.Clamp(Math.Round((nextPathPosition.X - GlobalPosition.X)/64),-1,1)*64;
+		delta.Y = (int)Math.Clamp(Math.Round((nextPathPosition.Y - GlobalPosition.Y)/64),-1,1)*64;
+		Translate(delta);
 	}
 }

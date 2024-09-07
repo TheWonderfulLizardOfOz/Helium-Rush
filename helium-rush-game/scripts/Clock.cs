60372253f52ef0148b3cdfe3220361de22ff4476
Haiwei1027 <71439492+Haiwei1027@users.noreply.github.com>
2024-09-07 16:17:40 +0100

    Add sleep need and game clock

---
+++ helium-rush-game/scripts/Clock.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class Clock : Label
+{
+	[Export]
+	//number of seconds a tick represent
+	public int tickDuration;
+
+	//number of ticks that has passed in this day
+	private int time;
+
+	public override void _Ready()
+	{
+		Timer timer = GetNode<Timer>("/root/Base/TickTimer");
+		timer.Timeout += Tick;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void Tick(){
+		time++;
+		Text = GetTimeString();
+	}
+
+	public string GetTimeString(){
+		string timeFormat = "{0}:{1}:{2}";
+		int seconds = time * tickDuration;
+		int minutes = seconds / 60;
+		int hours = minutes / 60;
+		return string.Format(timeFormat,hours%24, minutes%60, seconds%60);
+	}
+}
--- helium-rush-game/scripts/Pawn.cs
+++ helium-rush-game/scripts/Pawn.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Numerics;
 using Vector2 = Godot.Vector2;
 
@@ -8,14 +9,24 @@ public partial class Pawn : Node2D
 	[Export]
 	int sleepDecayRate = 10;
 
+	List<Need> needs = new List<Need>();
+
 	NavigationAgent2D navigationAgent;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var timer = GetNode<Timer>("/root/Base/TickTimer");
+		foreach (Node child in GetChildren()){
+			if (child is Need){
+				needs.Add(child as Need);
+				GD.Print(child.Name);
+			}
+		}
+
+		Timer timer = GetNode<Timer>("/root/Base/TickTimer");
+		timer.Timeout += Tick;
+
 		navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-		timer.Timeout += OnTick;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,13 +35,19 @@ public partial class Pawn : Node2D
 		navigationAgent.TargetPosition = GetGlobalMousePosition();
 	}
 
-	public void OnTick()
-	{	
+	public void Move(){
 		Vector2 delta;
 		Vector2 nextPathPosition = navigationAgent.GetNextPathPosition();
 		//Translate(nextPathPosition - currentPosition);
 		delta.X = (int)Math.Clamp(Math.Round((nextPathPosition.X - GlobalPosition.X)/64),-1,1)*64;
 		delta.Y = (int)Math.Clamp(Math.Round((nextPathPosition.Y - GlobalPosition.Y)/64),-1,1)*64;
 		Translate(delta);
+	}
+
+	public void Tick()
+	{	
+		foreach (Need need in needs){
+			need.Tick();
+		}
 	}
 }
